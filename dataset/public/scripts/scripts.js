dbb73f0c190ed5a86ade4601b001be45b9f9485f
Evan Turner <evturn@gmail.com>
2014-10-21 18:55:16 -0400

    Javascript entered into page

--- dataset/public/scripts/scripts.js
+++ dataset/public/scripts/scripts.js
@@ -1,14 +1,12 @@
 function onHover(event){
   var node = event.target;
   // Modify the node here! (e.g. color)
-  document.getElementById(liId).onmouseover = function() {ChangeColorBack(.id)
-
-
+ 	node.style.backgroundColor = "red";
 }
 
 // replace `data-class-name` with html class you use for a row of your data
 window.onload = function() {
-  var rows = document.getElementsByClassName('data-class-name');
+  var rows = document.getElementsByClassName('cool');
   for (var i = 0; i < rows.length; i++) {
     rows[i].addEventListener('mouseover', onHover);
   }
