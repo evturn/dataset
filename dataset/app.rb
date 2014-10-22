88799c8a832d1991d190e328bd0bb5cfc67c4f7a
Evan Turner <evturn@gmail.com>
2014-10-22 18:22:05 -0400

    All set but log in isn't logging in

--- dataset/app.rb
+++ dataset/app.rb
@@ -3,10 +3,55 @@ Bundler.require
 
 require_relative 'connection'
 require_relative 'models/subway'
+require_relative 'models/user'
 
+enable :sessions
 
 get '/' do
-	
-	@subways = Subway.all
+	@users = User.all
 	erb :index
 end
+
+get '/users/new' do
+	erb :'users/new'
+end
+
+post '/users' do
+	user = User.new(params[:user])
+	user.password = params[:password]
+	user.save!
+	redirect "/profiles"
+end
+
+get '/profiles' do
+	if session[:current_user]
+		@user = User.find(session[:current_user])
+		@subways = Subway.all
+	else
+		redirect '/'
+	erb :'profiles/index'
+	end
+end
+
+get '/sessions/new' do
+  erb :'sessions/new'
+end
+
+post '/sessions' do
+	user = User.find_by({username: params[:username]})
+  if user.password == params[:password]
+    session[:current_user] = user.id
+    redirect '/profiles'
+  else
+    redirect '/users/new'
+  end
+end
+
+delete '/sessions' do
+	session[:current_user] = nil
+	redirect 'users/new'
+end
+
+
+
+
--- dataset/db/schema.rb
+++ dataset/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141021203419) do
+ActiveRecord::Schema.define(version: 20141022214055) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 20141021203419) do
     t.string "url"
     t.string "name"
     t.string "line"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "username",      null: false
+    t.string   "password_hash", null: false
+    t.string   "avatar"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
